5aeb9a443940553cac00b52c581887be266ee3df
Ahmad Siraj MY <28752140+asmyio@users.noreply.github.com>
2021-09-02 01:41:50 +0800

    Solved Exercise Part 8 1/15

--- mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 
 public class Cubes {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter thee number");
+        
         while (true) {
             String input = scanner.nextLine();
+            
             if (input.equals("end")) {
                 break;
             }
+            
             int x = Integer.valueOf(input);
             System.out.println(x*x*x);
         }
