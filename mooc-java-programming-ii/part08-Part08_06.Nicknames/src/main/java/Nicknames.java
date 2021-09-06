72989456d5986ab969be3af534a2727a8b60d530
Ahmad Siraj MY <28752140+asmyio@users.noreply.github.com>
2021-09-07 01:59:54 +0800

    Solving the remaining questions

--- mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,12 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nicknamesMap = new HashMap<>();
+        nicknamesMap.put("matthew", "matt");
+        nicknamesMap.put("michael", "mix");
+        nicknamesMap.put("arthur", "artie");
+        
+        System.out.println(nicknamesMap.get("matthew"));
+        
     }
-
 }
